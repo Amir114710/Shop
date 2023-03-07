98d0b426db8f7dd26ad067761d5147565606dd6f
unknown <97888az@gmail.com>
2023-03-07 15:34:35 +0330

    created ckeditor

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,4 @@
-from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 from django.utils.text import slugify
 from account.models import User
@@ -34,7 +34,7 @@ class Product(models.Model):
     title = models.CharField(max_length=150 , null=True , blank=True , verbose_name="نام کالا")
     english_title = models.CharField(max_length=150 , null=True , blank=True , verbose_name="نام اینگلیسی کالا")
     slug = models.SlugField(null=True , blank=True)
-    discription = RichTextField(null=True , blank=True , verbose_name="توضیحات کوتاه")
+    discription = RichTextUploadingField(null=True , blank=True , verbose_name="توضیحات کوتاه")
     price = models.SmallIntegerField(null=True , blank=True , verbose_name="قیمت کالا")
     code = models.SmallIntegerField(null=True , blank=True , verbose_name="کد کالا" , unique=True)
     category = models.ManyToManyField(Category , verbose_name="دسته بندی محصول" , related_name="products")
